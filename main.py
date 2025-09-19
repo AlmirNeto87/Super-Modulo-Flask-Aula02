22b726e7f1c7bd3edd2b190a4f0018b9967cf344
Antonio Almir <149828632+AlmirNeto87@users.noreply.github.com>
2025-09-19 12:46:05 -0300

    Inclusao de comentarios no arquivo main.py

--- main.py
+++ main.py
@@ -8,35 +8,45 @@ lista_produtos = [
     {'id': 3, 'nome': 'Calça Jeans', 'preco': 30.00},
     {'id': 4, 'nome': 'Bermuda', 'preco': 250.00}
 ]
-
+#-----------------------------------------------------------------------------
 #Criacao de ROTAS (routes)
 #Para criar mais de uma rota para o mesmo caminho basta deixas juntas
 # funcao render_template e usada para renderizar pagina html
+#-----------------------------------------------------------------------------
 @app.route('/')
 @app.route('/home')
 def home():
 # E Possivel passar variaveis com descontrucao para a pagina que sta sendo rendenrizada 
   return render_template('index.html',titulo ="Home Page")
 
-
+#--------------------------------------------------------------------------------
+#Criacao de rota para a pagina sobre
+#--------------------------------------------------------------------------------
 @app.route('/sobre')
 def sobre():
   return render_template('sobre.html',titulo ="Sobre")
 
+#--------------------------------------------------------------------------------
+#Criacao de rota para a pagina contatos
+#--------------------------------------------------------------------------------
 @app.route('/contatos')
 def contatos():
   return render_template('contatos.html', titulo="Contatos")
 
-#Criacao de rota dinamica passando uma variavel '/rota/<tipo_variavel:variavel>'
-@app.route('/contato/<int:num>')
-def verNumero(num):
-  return f'O numero do Contato e {num}'
 
+#--------------------------------------------------------------------------------
+#Criacao de rota para a pagina produtos
 #Rota para exibir os produtos da lista , mandando a lista de produtos para a renderizacao do template
+#--------------------------------------------------------------------------------
 @app.route('/produtos')
 def listar_produtos():
   return render_template('produtos.html', titulo="Lista de Produtos", produtos=lista_produtos)
 
+#--------------------------------------------------------------------------------
+#Criacao de rota para cadastrar produtos  
+# Formulario para cadastrar produtos
+# Suporta métodos GET (exibir formulário) e POST (enviar formulário)
+#--------------------------------------------------------------------------------
 @app.route('/produtos/cadastro', methods=['GET', 'POST'])
 def cadastrar_produtos():
   if request.method == 'POST':
@@ -51,6 +61,11 @@ def cadastrar_produtos():
   
   return render_template('cadastrar_produto.html', titulo="Cadastrar Produto")
 
+#--------------------------------------------------------------------------------
+#Criacao de rota para editar produtos
+# Formulario para editar produtos
+# Suporta métodos GET (exibir formulário) e POST (enviar formulário)
+#--------------------------------------------------------------------------------
 @app.route('/produtos/editar/<int:id>', methods=['GET','POST'])
 def editar_produto(id):
   produtoNovo = None
@@ -66,6 +81,10 @@ def editar_produto(id):
     return redirect(url_for('listar_produtos'))
   return render_template('editar_produto.html', produto = produtoNovo, titulo="Editar Produto")
 
+#--------------------------------------------------------------------------------
+#Criacao de rota para deletar produtos
+# Tem como parametro o id do produto a ser deletado
+#--------------------------------------------------------------------------------
 @app.route('/produtos/deletar/<int:id>')
 def deletar_produto(id):
   produtoDeletado = None
@@ -78,18 +97,25 @@ def deletar_produto(id):
   lista_produtos.remove(produtoDeletado)
   return redirect(url_for('listar_produtos'))
 
-
+#--------------------------------------------------------------------------------
 # Cria uma rota para caminhos inexistentes 
 # Manipulador de erro 404 (Página não encontrada)
+#--------------------------------------------------------------------------------
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html', error_code=404, error_message="Página não encontrada."), 404
 
+#--------------------------------------------------------------------------------
+# Cria uma rota para erros internos do servidor
 # Manipulador de erro 500 (Erro interno do servidor)
+#--------------------------------------------------------------------------------
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('404.html', error_code=500, error_message="Erro interno no servidor. Tente novamente mais tarde."), 500
 
+
+#--------------------------------------------------------------------------------
 #Funcao para executar o flask 
+#--------------------------------------------------------------------------------
 if __name__ == "__main__":
     app.run(debug=True)
